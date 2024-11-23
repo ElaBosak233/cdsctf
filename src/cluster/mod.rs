20c5be0965b24333af3993cf98da1a1aea971ab8
埃拉 <ElaBosak233@qq.com>
2024-11-23 18:46:47 +0800

    fix: cluster

--- src/cluster/mod.rs
+++ src/cluster/mod.rs
@@ -93,10 +93,7 @@ pub async fn create(
                 name: name.clone(),
                 image: challenge.image_name.clone(),
                 env: Some(env_vars),
-                ports: Some(match crate::env::get_env().cluster.proxy.enabled {
-                    true => vec![],
-                    false => container_ports,
-                }),
+                ports: Some(container_ports),
                 ..Default::default()
             }],
             ..Default::default()
@@ -109,55 +106,68 @@ pub async fn create(
     kube::runtime::wait::await_condition(pod_api.clone(), &name, conditions::is_pod_running())
         .await?;
 
-    let service_api: Api<Service> = Api::namespaced(
-        client.clone(),
-        crate::env::get_env().cluster.namespace.as_str(),
-    );
-
-    let service = Service {
-        metadata: metadata.clone(),
-        spec: Some(ServiceSpec {
-            selector: Some(BTreeMap::from([(
-                String::from("cds/resource_id"),
-                name.clone(),
-            )])),
-            ports: Some(
-                challenge
-                    .ports
-                    .iter()
-                    .map(|port| ServicePort {
-                        port: *port,
-                        target_port: None,
-                        protocol: Some("TCP".to_string()),
-                        ..Default::default()
-                    })
-                    .collect(),
-            ),
-            type_: Some("NodePort".to_string()),
-            ..Default::default()
-        }),
-        ..Default::default()
-    };
-
-    service_api.create(&PostParams::default(), &service).await?;
-
-    let service = service_api.get(&name).await?;
-
     let mut nats: Vec<crate::db::entity::pod::Nat> = Vec::new();
-    if let Some(spec) = service.spec {
-        if let Some(ports) = spec.ports {
-            for port in ports {
-                if let Some(node_port) = port.node_port {
-                    nats.push(crate::db::entity::pod::Nat {
-                        src: format!("{}", port.port),
-                        dst: Some(format!("{}", node_port)),
-                        proxy: crate::env::get_env().cluster.proxy.enabled,
-                        entry: Some(format!(
-                            "{}:{}",
-                            crate::config::get_config().await.cluster.entry,
-                            node_port
-                        )),
-                    });
+
+    match crate::env::get_env().cluster.proxy.enabled {
+        true => {
+            for port in challenge.ports {
+                nats.push(crate::db::entity::pod::Nat {
+                    src: format!("{}", port),
+                    dst: None,
+                    proxy: crate::env::get_env().cluster.proxy.enabled,
+                    entry: None,
+                });
+            }
+        },
+        false => {
+            let service_api: Api<Service> = Api::namespaced(
+                client.clone(),
+                crate::env::get_env().cluster.namespace.as_str(),
+            );
+            let service_ports: Vec<ServicePort> = challenge
+                .ports
+                .iter()
+                .map(|port| ServicePort {
+                    port: *port,
+                    target_port: None,
+                    protocol: Some("TCP".to_string()),
+                    ..Default::default()
+                })
+                .collect();
+
+            let service = Service {
+                metadata: metadata.clone(),
+                spec: Some(ServiceSpec {
+                    selector: Some(BTreeMap::from([(
+                        String::from("cds/resource_id"),
+                        name.clone(),
+                    )])),
+                    ports: Some(service_ports),
+                    type_: Some("NodePort".to_string()),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            };
+
+            service_api.create(&PostParams::default(), &service).await?;
+
+            let service = service_api.get(&name).await?;
+            if let Some(spec) = service.spec {
+                if let Some(ports) = spec.ports {
+                    for port in ports {
+                        if let Some(node_port) = port.node_port {
+                            nats.push(crate::db::entity::pod::Nat {
+                                src: format!("{}", port.port),
+                                dst: Some(format!("{}", node_port)),
+                                proxy: crate::env::get_env().cluster.proxy.enabled,
+                                entry: Some(format!(
+                                    "{}:{}",
+                                    crate::config::get_config().await.cluster.entry,
+                                    node_port
+                                )),
+                            });
+                        }
+                    }
                 }
             }
         }
