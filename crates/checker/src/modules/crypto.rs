f1198c3c4f39cefe1aebcecb3a7ffaef2c40024c
Ela <i@e23.dev>
2025-01-31 22:02:49 +0800

    wip: checker with rune

---
+++ crates/checker/src/modules/crypto.rs
@@ -0,0 +1,24 @@
+use ring::digest::{SHA256, SHA512};
+use rune::{ContextError, Module};
+
+#[rune::module(::crypto)]
+pub fn module(_stdio: bool) -> Result<Module, ContextError> {
+    let mut module = Module::from_meta(module_meta)?;
+    module.function_meta(sha256)?;
+
+    Ok(module)
+}
+
+#[rune::function]
+pub fn sha256(message: &str) -> String {
+    let mut context = ring::digest::Context::new(&SHA256);
+    context.update(message.as_bytes());
+    hex::encode(context.finish().as_ref())
+}
+
+#[rune::function]
+pub fn sha512(message: &str) -> String {
+    let mut context = ring::digest::Context::new(&SHA512);
+    context.update(message.as_bytes());
+    hex::encode(context.finish().as_ref())
+}
