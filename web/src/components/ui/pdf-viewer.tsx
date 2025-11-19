05a0626751966384117cda996fa363762a4fef2f
Ela <i@e23.dev>
2025-11-19 10:20:12 +0800

    fix: pdf-viewer

--- web/src/components/ui/pdf-viewer.tsx
+++ web/src/components/ui/pdf-viewer.tsx
@@ -5,10 +5,7 @@ import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
 import { LoaderCircleIcon } from "lucide-react";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 export interface PDFViewerProps {
   url: string;
