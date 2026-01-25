6d82bcb3ceefbbb3714b59cb94f54138d2929b2d
Ela <i@e23.dev>
2026-01-25 17:26:50 +0800

    chore: adjust database connection pool timeouts

--- crates/db/src/lib.rs
+++ crates/db/src/lib.rs
@@ -43,9 +43,9 @@ pub async fn init(env: &Env) -> Result<DB, DbError> {
     opt.max_connections(100)
         .min_connections(5)
         .connect_timeout(Duration::from_secs(8))
-        .acquire_timeout(Duration::from_secs(8))
-        .idle_timeout(Duration::from_secs(8))
-        .max_lifetime(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(2))
+        .idle_timeout(Duration::from_mins(10))
+        .max_lifetime(Duration::from_mins(30))
         .sqlx_logging(true)
         .sqlx_logging_level(log::LevelFilter::Debug)
         .set_schema_search_path("public");
